abbba57d518b2133476bc61c53139bbf34ace4fd
hacen27 <hacenm144@gmail.com>
2022-11-09 21:18:00 +0000

    3teamsprojet

--- tebarrou3/settings.py
+++ tebarrou3/settings.py
@@ -143,17 +143,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 # django_heroku.sttings(locals())
-django_heroku.settings(locals())
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
