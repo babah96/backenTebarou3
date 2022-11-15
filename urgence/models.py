dfee9cdad61fa134371b2eac957e6d584f6ca8fd
hacen27 <hacenm144@gmail.com>
2022-11-15 23:47:57 +0000

    firstdata

--- urgence/models.py
+++ urgence/models.py
@@ -5,4 +5,12 @@ class Urgence(models.Model):
     lantitude= models.DecimalField(max_length=10,max_digits=9, decimal_places=7)
     longitude=models.DecimalField(max_length=10,max_digits=9, decimal_places=7)
     descreption=models.TextField(null=True, blank=True)
-    image=models.ImageField()
+    image=models.ImageField(verbose_name='description')
+
+def image(self):
+    if self.image:
+        return u'<img src="%s" width="50" height="50" />' % self.image.url
+    else:
+        return '(Sin imagen)'
+image.short_description = 'image'
+image.allow_tags = True
